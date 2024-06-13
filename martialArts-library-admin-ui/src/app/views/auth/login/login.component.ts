cba36a90a9e81ea0b18645a27693ae18b9139e5d
Phan Thien Phu <phanthienphu199@gmail.com>
2024-06-14 00:38:08 +0700

    add loading, sign out

--- martialArts-library-admin-ui/src/app/views/auth/login/login.component.ts
+++ martialArts-library-admin-ui/src/app/views/auth/login/login.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule  } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AdminApiAuthApiClient, AuthenticatedResult, LoginRequest } from 'src/app/api/admin-api.service.generated';
 import { AlertService } from 'src/app/shared/services/alert.service';
 import {UrlConstants} from 'src/app/shared/constants/url.constants'
 import { TokenStorageService } from 'src/app/shared/services/token-storage.service';
+import { Subject, take, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy{
   loginForm:FormGroup;
+  private ngUnsubscribe = new Subject<void>();
+  loading = false;
+
   constructor(private fb:FormBuilder,
     private authApiClient:AdminApiAuthApiClient,
     private alertService:AlertService,
@@ -23,13 +27,20 @@ export class LoginComponent {
       passWord: new FormControl('',Validators.required)
     });
   }
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
   login(){
+    this.loading=true;
     var request: LoginRequest = new LoginRequest({
       userName:this.loginForm.controls['userName'].value,
       password:this.loginForm.controls['passWord'].value
     });
 
-    this.authApiClient.login(request).subscribe({
+    this.authApiClient.login(request)
+    .pipe(takeUntil(this.ngUnsubscribe))
+    .subscribe({
       next:(res:AuthenticatedResult) =>{
         //save token and refresh token to localstorege
         this.tokenService.saveToken(res.token);
@@ -40,7 +51,8 @@ export class LoginComponent {
       },
       error:(error:any)=>{
         console.log(error);
-        this.alertService.showError('Đăng nhập không đúng!')
+        this.alertService.showError('Đăng nhập không đúng!');
+        this.loading = false;
       }
     });
   }
